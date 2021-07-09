da64dc6296cdb03706ab47c6dc387a984bcecaaa
Thanassis Frank <thanassisfrank@gmail.com>
2021-07-09 13:57:45 +0100

    Converted to Data class

--- data.js
+++ data.js
@@ -8,6 +8,8 @@ function Data() {
     this.normals = [];
     this.maxSize = 0;
     this.midPoint = [];
+    this.size = [];
+    this.cellSize = [1, 1, 1];
     this.generateData = function(x, y, z, f) {
         var data = [];
         for (let i = 0; i < x; i++) {
@@ -15,13 +17,18 @@ function Data() {
             for (let j = 0; j < y; j++) {
                 data[i][j] = [];
                 for (let k = 0; k < z; k++) {
-                    data[i][j].push(f(i, j, k));
+                    // values are clamped to > 0
+                    data[i][j].push(Math.max(0, f(i, j, k)));
                 }
             }
         }
         this.data = data;
         this.maxSize = Math.max(x, y, z);
-        this.midPoint = [(x-1)/2, (y-1)/2, (z-1)/2]
+        this.midPoint = [(x-1)/2, (y-1)/2, (z-1)/2];
+        this.size = [x, y, z];
         return this.data;
-    }
+    };
+    this.setCellSize = function(size) {
+        this.cellSize = size;
+    };
 }
